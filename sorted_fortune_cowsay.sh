059c6d38f5514f52a88b4094936914dbf696011b
Caio Bueno <33076908+caioabueno@users.noreply.github.com>
2023-10-27 23:54:50 -0300

    Update sorted_fortune_cowsay.sh / adicionado comentarios

--- sorted_fortune_cowsay.sh
+++ sorted_fortune_cowsay.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+#'sed' tira as strings sequenciais que não queremos e 'tr' troca um caractere por outro, no caso o espaço foi trocado por quebra de linha
 cows="$(cowsay -l | sed 's\Cow files in /usr/share/cowsay/cows:\\' | tr ' ' '\n')"
-
+#'shuf -n 1' traz um unico elemento da variavel cows aleatóriamente
 cow_aleatoria=$(echo "$cows" | shuf -n 1)
 
 echo $cow_aleatoria
